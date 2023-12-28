651d8bd10015e2645b312313ac9e8ebe461847a1
drewburr <drew@drewburr.com>
2023-12-28 17:57:39 -0500

    Translate project to use python-spacetraders

--- spacetraders-client/client/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from client.api.agents_api import AgentsApi
-from client.api.contracts_api import ContractsApi
-from client.api.factions_api import FactionsApi
-from client.api.fleet_api import FleetApi
-from client.api.systems_api import SystemsApi
-from client.api.default_api import DefaultApi
